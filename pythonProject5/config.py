324fa4a269b84646167fcace7a43896d0bfdc95f
YK-YANG-Z <15083459499@163.com>
2025-12-29 04:36:55 +0800

    Add files via upload

---
+++ pythonProject5/config.py
@@ -0,0 +1,7 @@
+# 剧社资料查询系统 - 数据库配置文件
+# ============ 此处修改为你的MySQL真实账号密码 ============
+DB_HOST = "localhost"    # 本地数据库默认localhost，不用改
+DB_PORT = 3306           # MySQL默认端口，不用改
+DB_USER = "root"         # 你的MySQL登录账号，一般是root
+DB_PASSWORD = "YKsgdsg2@"   # 你的MySQL登录密码，必填修改！
+DB_NAME = "drama_club"   # 数据库名，和上面建表的库名一致，不用改
